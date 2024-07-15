5fe1fe3f505ae161ee70550d8f6248a9b122c35a
Lee Cook <leethomascook@hotmail.co.uk>
2024-07-15 22:41:49 +0100

    Update countdown.js

--- public/countdown.js
+++ public/countdown.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   // Month Day, Year Hour:Minute:Second, id-of-element-container
-  countUpFromTime("Jan 1, 2014 12:00:00", 'countup1'); // ****** Change this line!
+  countUpFromTime("July 30, 2014 17:30:00", 'countup1'); // ****** Change this line!
 };
 function countUpFromTime(countFrom, id) {
   countFrom = new Date(countFrom).getTime();
